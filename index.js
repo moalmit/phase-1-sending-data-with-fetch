d4f1ded462e7c5894e907f0bfedfa122a2908eca
Mohammed Al Mitwalli <mohammed.almitwalli@gmail.com>
2023-04-14 12:09:40 -0700

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Add your code here
+function submitData (a, b){
+
+    const obj = {name: a,
+                 email: b,
+                }; 
+    const object = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(obj),
+    };
+
+return fetch('http://localhost:3000/users', object)
+
+.then(function(response){
+    return response.json();   
+})
+
+.then(function(data){
+        let p = document.createElement("p");
+        p.innerHTML = data.id;
+        document.body.appendChild(p);
+    return 
+})
+
+
+.catch(function(error){
+    let p = document.createElement('p');
+    p.innerHTML = 'Unauthorized Access';
+    document.body.appendChild(p);
+    
+    return
+});
+
+}
+
+submitData("steve", "steve@steve.com");
